98d257ce7ecc18be6be283e9750facb2defa1ba2
Glumbit <footbolka46@gmail.com>
2023-06-05 20:21:51 +0300

    ver 2.0

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,80 +1,113 @@
-$(document).ready(() => {
+const mechanism = document.querySelectorAll('.mechanism__item');
+let counter = 360;
 
-	let mechanism = $('.mechanism__item');
-	let counter = 360;
-	$(window).scroll(function () {
-		setTimeout(() => {
-			counter = counter + 5;
-			for (const i in mechanism) {
-				if (Object.hasOwnProperty.call(mechanism, i)) {
-					const gear = mechanism[i];
-					if (i % 2 === 0) {
-						$(gear).css('transform', `rotate(${counter}deg`);
-					}
-					if (i % 2 !== 0) {
-						$(gear).css('transform', `rotate(-${counter}deg`);
+window.addEventListener("wheel", mechanismRotate);
+
+function mechanismRotate(event) {
+	if (event.deltaY > 0) {
+		counter += 10;
+		for (const key in mechanism) {
+			if (Object.hasOwnProperty.call(mechanism, key)) {
+				const mechanismEl = mechanism[key];
+				if (key % 2 !== 0) {
+					mechanismEl.style.transform = `rotate(${counter}deg`;
+				}
+				if (key % 2 === 0) {
+					mechanismEl.style.transform = `rotate(-${counter}deg`;
+				}
+			}
+		}
+	}
+	else {
+		counter -= 10;
+		for (const key in mechanism) {
+			if (Object.hasOwnProperty.call(mechanism, key)) {
+				const mechanismEl = mechanism[key];
+				if (key % 2 !== 0) {
+					mechanismEl.style.transform = `rotate(${counter}deg`;
+				}
+				if (key % 2 === 0) {
+					mechanismEl.style.transform = `rotate(-${counter}deg`;
+				}
+			}
+		}
+	}
+}
+
+const burger = document.querySelector('.burger__inner');
+const navItem = document.querySelector('.nav__item');
+
+burger.addEventListener("click", navShow);
+navItem.addEventListener("click", navShow);
+
+function navShow() {
+	document.querySelector('.burger__inner').classList.toggle('burger__inner--active');
+	document.querySelector('.nav__inner').classList.toggle('nav__inner--visible');
+}
+
+const aboutAnim = new IntersectionObserver(
+	(entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				document.querySelector('#aside__item-left').style.transform = "translate(-50%, 0) rotate(90deg)";
+				document.querySelector('#aside__item-right').style.transform = "translate(50%, 0) rotate(90deg)";
+				document.querySelector('#about__img').style.transform = "translate(0)";
+				document.querySelector('#about__text').style.transform = "translate(0)";
+			}
+		});
+	}
+);
+
+aboutAnim.observe(document.querySelector('.about__inner'));
+
+document.querySelector('#btn__works').addEventListener("click", showWorks);
+
+function showWorks() {
+	const worksItems = document.querySelectorAll('.works__item');
+	for (const i in worksItems) {
+		if (Object.hasOwnProperty.call(worksItems, i)) {
+			const worksItem = worksItems[i];
+			worksItem.style.display = "block";
+			document.querySelector('#btn__works').style.display = "none";
+		}
+	}
+}
+
+const stagesAnim = new IntersectionObserver(
+	(entries) => {
+		entries.forEach(entry => {
+			if (entry.isIntersecting) {
+				const stagesItems = document.querySelectorAll('.stages__item');
+				for (const i in stagesItems) {
+					if (Object.hasOwnProperty.call(stagesItems, i)) {
+						const stagesItem = stagesItems[i];
+						setTimeout(() => {
+							stagesItem.style.transform = "translate(0)"
+						}, 100)
 					}
 				}
 			}
-		}, 100);
-
-	});
-
-	$('.burger__inner').click(burgerAction);
-	$('.nav__item').click(burgerAction);
-	function burgerAction() {
-		$('.burger__inner').toggleClass('burger__inner--active');
-		$('.nav__inner').toggleClass('nav__inner--visible');
-	}
-
-	let worksHeight = $('.works__content').find('.works__item').eq(0).outerHeight(true);
-	$('.works__content').css({ maxHeight: `${worksHeight}px` });
-	$('#btn_works').click(function () {
-		$(this).css(`display`, 'none').prev().css({ maxHeight: `none` });
-	});
-
-	$('textarea').keydown(function () {
-		$(this).height(function () {
-			console.log(this);
-			return $(this).height() + $(this).scrollTop();
 		});
-	});
+	}
+);
 
-	const aboutAnim = new IntersectionObserver(
-		(entries) => {
-			entries.forEach(entry => {
-				if (entry.isIntersecting) {
-					$('#aside__item-left').css('transform', 'translate(-50%, 0) rotate(90deg)');
-					$('#aside__item-right').css('transform', 'translate(50%, 0) rotate(90deg)');
-					$('#about__img').css('transform', 'translate(0)');
-					$('#about__text').css('transform', 'translate(0)');
-				}
-			});
-		}
-	);
-	$('.about__inner').each(function () {
-		aboutAnim.observe(this);
-	});
+stagesAnim.observe(document.querySelector('.stages'));
 
-	const stagesAnim = new IntersectionObserver(
-		(entries) => {
-			setTimeout(() => {
-				entries.forEach(entry => {
-					if (entry.isIntersecting) {
-						$(entry.target).css('transform', 'translate(0)');
-					}
-				});
-			}, 100);
+const textarea = document.querySelector('textarea');
+textarea.addEventListener("keypress", textareaSize);
 
+function textareaSize() {
+	textarea.style.height = `${textarea.scrollHeight}px`;
+}
 
-		}
-	);
-	$('.stages__item').each(function () {
-		stagesAnim.observe(this);
-	})
-	$('.questions__details').slideUp();
-	$('.questions__item').click(function () {
-		$(this).find('.questions__details').slideToggle(500);
-		$(this).find('.arrow-questions').toggleClass('rotate');
-	})
-});
+const questionsItems = document.querySelectorAll('.questions__item');
+for (const i in questionsItems) {
+	if (Object.hasOwnProperty.call(questionsItems, i)) {
+		const questionsItem = questionsItems[i];
+		questionsItem.addEventListener("click", questionShow.bind(null, questionsItem))
+	}
+}
+
+function questionShow(questionItem) {
+	questionItem.classList.toggle("questions__item--visible")
+}
